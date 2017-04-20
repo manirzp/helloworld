2989d5d50521261b5811d152c3dfb3fb941e8a63
Manikandangithub-4991 <greatmani1994@gmail.com>
2017-04-20 15:36:21 +0530

    Added TestA file

---
+++ TestA.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestA(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+    def test_something1(self):
+        self.assertEqual(True, True)
+
+
+    @staticmethod
+    def suite():
+        """TestSuite for this class"""
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestA)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestA)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
